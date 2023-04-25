17506459fda806bcf6aac4d67ae936d2ad4d3e40
Xavier DAVID <xavier.david@whitesmokegames.com>
2023-04-25 16:53:48 +0200

    MODIF MeshRenderer classes -> now works with the gameEngine

--- src/MeshRenderer.cpp
+++ src/MeshRenderer.cpp
@@ -1,16 +1,82 @@
 #include "MeshRenderer.h"
+#include "Game.h"
+#include "Scene.h"
+#include "GameObject.h"
+#include "Transform.h"
 
 namespace XDGameEngine
 {
 	MeshRenderer::MeshRenderer()
 	{
 		m_id = 'MSRD';
+		m_meshFileName = nullptr;
+
+		m_scnMgr = nullptr;
+		m_mesh = nullptr;
+		m_sceneNode = nullptr;
 		std::cout << " MESH RENDERER CREATED\n";
 	}
 
 	MeshRenderer::~MeshRenderer()
 	{
+		m_scnMgr->destroyMovableObject(m_mesh);
+		m_scnMgr = nullptr;
+
+		//if (m_mesh != nullptr)
+		//	delete m_mesh;
+		//m_mesh = nullptr;
+
+		//if (m_sceneNode != nullptr)
+		//	delete m_sceneNode;
+		//m_sceneNode = nullptr;
 		std::cout << " MESH RENDERER DESTRUCTED\n";
+	}
+
+	void MeshRenderer::SetUpComponent()
+	{
+		m_scnMgr = Game::Instance()->getCurrentScene()->getSceneManager();
+
+		if (m_meshFileName == nullptr)
+			return;
+		m_mesh = m_scnMgr->createEntity(m_meshFileName);
+		m_sceneNode = m_scnMgr->getRootSceneNode()->createChildSceneNode();
+		m_sceneNode->attachObject(m_mesh);
+		m_sceneNode->setScale(1.0f, 1.0f, 1.0f);
+	}
+
+	void MeshRenderer::UpdateComponent(GameObject& go)
+	{
+		Transform* trans = go.GetTransform();
+		btQuaternion rot = trans->getRotation();
+		if (go.ShouldBeScaled() == true)
+		{
+			float x = trans->getScale().getX();
+			float y = trans->getScale().getY();
+			float z = trans->getScale().getZ();
+			m_sceneNode->setScale(x, y, z);
+			go.ResetShouldBeScaled();
+		}
+
+		m_sceneNode->setPosition(Ogre::Vector3(trans->getPosition().getX(),
+			trans->getPosition().getY(), trans->getPosition().getZ()));
+		m_sceneNode->setOrientation(Ogre::Quaternion(rot.getW(), rot.getX(),
+			rot.getY(), rot.getZ()));
+	}
+
+
+	void MeshRenderer::SetActive(bool isActive)
+	{
+		if (isActive == true && m_isActive == false)
+		{
+			if (m_sceneNode != nullptr)
+				m_sceneNode->setVisible(true);
+		}
+		else if (isActive == false && m_isActive == true)
+		{
+			if (m_sceneNode != nullptr)
+				m_sceneNode->setVisible(false);
+		}
+		m_isActive = isActive;
 	}
 
 	void MeshRenderer::printUnAutreTruc()
@@ -18,4 +84,33 @@ namespace XDGameEngine
 		std::cout << "MESH PRINT UN AUTRE TRUC\n";
 	}
 
+	void MeshRenderer::SetMeshFileName(const char* name) noexcept
+	{
+		m_meshFileName = name;
+	}
+
+	void MeshRenderer::SetMesh(Ogre::Entity* mesh) noexcept
+	{
+		m_mesh = mesh;
+	}
+
+	void MeshRenderer::SetSceneNode(Ogre::SceneNode* sceneNode) noexcept
+	{
+		m_sceneNode = sceneNode;
+	}
+
+	const char* MeshRenderer::GetMeshFileName() const noexcept
+	{
+		return m_meshFileName;
+	}
+
+	Ogre::Entity* MeshRenderer::GetMesh() const noexcept
+	{
+		return m_mesh;
+	}
+
+	Ogre::SceneNode* MeshRenderer::GetSceneNode() const noexcept
+	{
+		return m_sceneNode;
+	}
 }
--- src/MeshRenderer.h
+++ src/MeshRenderer.h
@@ -3,17 +3,35 @@
 
 #include "AComponent.h"
 
+#include "Ogre.h"
+
 namespace XDGameEngine
 {
-	class MeshRenderer : public XDGameEngine::AComponent
+	class MeshRenderer : public AComponent
 	{
 	public:
 		MeshRenderer();
 		~MeshRenderer();
 
+		virtual void SetUpComponent();
+		virtual void SetActive(bool isActive);
+		virtual void UpdateComponent(GameObject& go);
 		void printUnAutreTruc();
 
+		void SetMeshFileName(const char* name) noexcept;
+		void SetMesh(Ogre::Entity* mesh) noexcept;
+		void SetSceneNode(Ogre::SceneNode* sceneNode) noexcept;
+
+		const char* GetMeshFileName() const noexcept;
+		Ogre::Entity* GetMesh() const noexcept;
+		Ogre::SceneNode* GetSceneNode() const noexcept;
+
+
 	private:
+		Ogre::SceneManager* m_scnMgr;
+		const char* m_meshFileName;
+		Ogre::Entity* m_mesh;
+		Ogre::SceneNode* m_sceneNode;
 
 	protected:
 	};
